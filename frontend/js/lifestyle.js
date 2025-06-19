cd49fe024de043e9f047edc5ef8c1a0f9879509f
Paul <53634603+nothing36@users.noreply.github.com>
2025-06-19 16:28:13 -0600

    Add donut risk chart

--- frontend/js/lifestyle.js
+++ frontend/js/lifestyle.js
@@ -2,7 +2,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form    = document.getElementById('risk-form');
     const riskVal = document.querySelector('.risk-val');
+    const riskGaugeCanvas = document.getElementById('risk-gauge');
     const API_URL = 'http://localhost:8000/predict';
+
+    // change colour based on risk
+    function colour(r) {
+        return r < 0.15 ? '#4caf50' : r < 0.30 ? '#ffc107': '#f44336';
+    }
+
+    // add gauge
+    const riskGauge = new Chart(riskGaugeCanvas, {
+        type: 'doughnut',
+        data: { datasets: [{ data: [0, 1], backgroundColor: ['#e0e0e0','#e0e0e0'], borderWidth: 0, cutout: '65%', rotation: -90, circumference: 180 }] },
+        options: {
+            responsive: false,
+            plugins: {
+                tooltip: { enabled: false },
+                legend: { display: false },
+                subtitle: {
+                    display: true,
+                    text: 'Some Fields Empty',
+                    font: { size: 24, weight: 'bold' },
+                    position: 'bottom',
+                    color: '#F02D3A',
+                }
+            }
+        }
+    });
+
 
     async function maybeSend() {
         if (!form.checkValidity()) return;
@@ -37,7 +64,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 return;
             }
             const { risk } = await res.json();
-            riskVal.textContent = `${(risk * 100).toFixed(1)} %`;
+
+            // gauge and text update
+            const pct = (risk * 100).toFixed(1);
+            riskGauge.data.datasets[0].data = [risk, 1 - risk];
+            riskGauge.data.datasets[0].backgroundColor = [colour(risk), '#e0e0e0'];
+            riskGauge.options.plugins.subtitle.color = colour(risk);
+            riskGauge.options.plugins.subtitle.text = `${pct}%`;
+            riskGauge.update();
         } catch (err) {
             console.error(err);
         }
